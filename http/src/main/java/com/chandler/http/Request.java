a18d3c3232c960d7227b8139410e4473a1fe8c23
weijie <weijie.wang@tinno.com>
2018-03-23 15:56:27 +0800

    init

---
+++ http/src/main/java/com/chandler/http/Request.java
@@ -0,0 +1,28 @@
+package com.chandler.http;
+
+import java.util.Map;
+
+/**
+ * Created by weijie on 18-3-23.
+ */
+
+public class Request {
+    public enum RequestMethod {GET, POST, PUT, DELETE}
+
+
+    public String url;
+    public String content;
+    public Map<String, String> headers;
+
+    public RequestMethod method;
+
+    public Request(String url,RequestMethod method){
+        this.url = url;
+        this.method = method;
+    }
+
+    public Request(String url){
+        this.url = url;
+        this.method = RequestMethod.GET;
+    }
+}
