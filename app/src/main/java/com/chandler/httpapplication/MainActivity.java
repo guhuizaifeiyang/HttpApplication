8ac9fc18678455ce952bb3b4a880ad213bba7a2c
weijie <weijie.wang@tinno.com>
2018-03-24 11:49:56 +0800

    FileCallback : download file to defined path by chandler 1

--- app/src/main/java/com/chandler/httpapplication/MainActivity.java
+++ app/src/main/java/com/chandler/httpapplication/MainActivity.java
@@ -1,11 +1,16 @@
 package com.chandler.httpapplication;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.chandler.http.FileCallback;
 import com.chandler.http.ICallback;
@@ -15,8 +20,13 @@ import com.chandler.http.RequestTask;
 
 import java.io.File;
 
+import static android.support.v4.app.ActivityCompat.requestPermissions;
+import static android.support.v4.content.ContextCompat.checkSelfPermission;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final int REQUEST_CODE_PERMISSIONS = 123;
+    private static final String TAG = "chandler";
     private Button mButton;
 
     @Override
@@ -83,6 +93,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void testHttpPostForDownload() {
+        checkPermissions();
+    }
+
+    private void testHttpPostForDownloadInter() {
         String url = "http://api.stay4it.com/v1/public/core/?service=user.login";
         String content = "account=stay4it&password=123456";
         String path = Environment.getExternalStorageDirectory() + File.separator + "demo.txt";
@@ -91,7 +105,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             @Override
             public void onSuccess(String result) {
-                Log.d("chandler", "testHttpGet return:" + result);
+                Log.d(TAG, "testHttpGet return:" + result);
             }
 
             @Override
@@ -102,5 +116,51 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         request.content = content;
         RequestTask task = new RequestTask(request);
         task.execute();
+    }
+
+    /**
+     * @param
+     * @return
+     * Request read and write permissions
+     */
+    private void checkPermissions() {
+        int hasWritePermission = checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int hasReadPermission = checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE);
+        if (hasWritePermission != PackageManager.PERMISSION_GRANTED || hasReadPermission != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[] {Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        REQUEST_CODE_PERMISSIONS);
+            }
+        } else {
+            testHttpPostForDownloadInter();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_PERMISSIONS:
+                boolean allPermissionGrant = true;
+                for (int i = 0; i < permissions.length; i++) {
+                    Log.d(TAG, "onRequestPermissionsResult: request permisiion = " + permissions[i]);
+                    if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
+                        allPermissionGrant = false;
+                    }
+
+                }
+                if (allPermissionGrant) {
+                    // Permission Granted
+                    // do something
+                    testHttpPostForDownloadInter();
+                } else {
+                    // Permission Denied
+                    Toast.makeText(MainActivity.this, "WRITE_CONTACTS Denied", Toast.LENGTH_SHORT)
+                            .show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        }
     }
 }
